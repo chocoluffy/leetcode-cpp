24164245fd8dc905aaa3de33d6452e8c6bf38835
chocoluffy <yushunzhe951104@gmail.com>
2018-08-11 16:37:09 +0800

    Update q7.6

--- EPI/q7-6-even-odd-merge.cpp
+++ EPI/q7-6-even-odd-merge.cpp
@@ -1,4 +1,5 @@
-// given a list <l0, l1, l2, ...>, modify it into <l0, l2, l4,..., l1, l3...> a even-odd merged list.
+// Q: given a list <l0, l1, l2, ...>, modify it into <l0, l2, l4,..., l1, l3...> a even-odd merged list.
+// 多指针同时遍历的技巧。prev的使用。以及corner case针对even\odd的讨论。一个技巧是，根据已有的变量是否会seg fault来判断需要新增的case，最常见的遗漏就是对是否为null的讨论（针对linked list是否为空等等）。
 node* even_odd_merge(node* L) {
   node* prev_even = NULL;
   node* even = L, odd = L.next;
@@ -11,7 +12,7 @@ node* even_odd_merge(node* L) {
       odd.next = even.next;
       odd = odd.next;
     }
-  }      
+  }  	
   if(even) { // case: odd number of elements in list.
     prev_even = even;
   }
@@ -19,4 +20,4 @@ node* even_odd_merge(node* L) {
     prev_even.next = odd_start;
   }
   return L;
-}   
+}	
