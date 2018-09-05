b4183ffd87f87843dd14fb6670977a0e7e4b5212
chocoluffy <yushunzhe951104@gmail.com>
2018-09-05 18:00:34 -0400

    Update Q200 solution

--- Leetcode/200-Number-Of-Islands/solution.py
+++ Leetcode/200-Number-Of-Islands/solution.py
@@ -69,6 +69,16 @@ class Solution(object):
                     cnt += 1
                     dfs(i, j)
         return cnt
+    
+    # clever use of `map`
+    def numIslands_v3(self, grid):
+        def sink(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+                grid[i][j] = '0'
+                map(sink, (i+1, i-1, i, i), (j, j, j+1, j-1)) # recur the function sink with the following pairs.
+                return 1
+            return 0
+        return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
 
 grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
 print Solution().numIslands(grid)
