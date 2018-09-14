d477e21c70f6e84e5568e96417d0be389ed6f164
chocoluffy <yushunzhe951104@gmail.com>
2018-09-13 21:57:53 -0400

    Update Q722

--- Leetcode/722-Remove-Comments/solution.py
+++ Leetcode/722-Remove-Comments/solution.py
@@ -1,10 +1,15 @@
 """
 
-record valid /* start index, and mark delete mode; 
-after each string, if not end index, then delete
-till the end. and mark the start index = 0 (meaning for all later string, start
-deleting from the start)
+Input:
+["a/*comment", "line", "more_comment*/b"]
+Output:
+["a","b"]
+Expected:
+["ab"]
 
+
+A good edge case:
+meaning that we only create new empty new_s [], when it's in search mode.
 """
 class Solution(object):
     def removeComments(self, source):
@@ -12,9 +17,62 @@ class Solution(object):
         :type source: List[str] 
         :rtype: List[str]
         """
+        res = []
+        mode = 0 # 0: search mode, 1: delete mode.
         for s in source:
             n = len(s)
-            for i in range(0, n-1): # mind the last char.
-                if s[i] == '/' and s[i+1] == '/'
+            if mode == 0:
+                new_s = []  
+            i = 0
+            stop = False
+            while i < n-1: # mind the last char.
+                if s[i] == '/' and s[i+1] == '/' and mode == 0:
+                    res.append("".join(new_s))
+                    stop = True
+                    break
+                elif s[i] == '/' and s[i+1] == '*' and mode == 0:
+                    # switch to delete mode.
+                    mode = 1
+                    i += 1
+                elif s[i] == '*' and s[i+1] == '/' and mode == 1:
+                    # switch to search mode.
+                    mode = 0
+                    i += 2
+                else: # other cases. 
+                    if mode == 0:
+                        new_s.append(s[i])
+                    i += 1
+            if i == n-1 and mode == 0 and not stop:
+                new_s.append(s[i])
+            if not stop:
+                string = "".join(new_s)
+                if len(string) > 0:
+                    res.append(string)
+        return res
 
-        
+
+# v2
+class Solution(object):
+    def removeComments(self, source):
+        in_block = False
+        ans = []
+        for line in source:
+            i = 0
+            if not in_block:
+                newline = []
+            while i < len(line):
+                if line[i:i+2] == '/*' and not in_block:
+                    in_block = True
+                    i += 1
+                elif line[i:i+2] == '*/' and in_block:
+                    in_block = False
+                    i += 1
+                elif not in_block and line[i:i+2] == '//':
+                    break
+                elif not in_block:
+                    newline.append(line[i])
+                i += 1
+            if newline and not in_block:
+                ans.append("".join(newline))
+
+        return ans
